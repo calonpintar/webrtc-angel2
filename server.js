b4928af1a35b1873cde418190d9f00a189faede7
calonpintar <52047665+calonpintar@users.noreply.github.com>
2020-06-11 03:42:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,3 @@
-// Copyright 2017 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-'use strict';
 const HTTPS_PORT = 8080;
 
 const fs = require('fs');
@@ -20,35 +5,28 @@ const https = require('https');
 const WebSocket = require('ws');
 const WebSocketServer = WebSocket.Server;
 
-// [START gae_node_request_example]
-const express = require('express');
-
-const app = express();
-
 // Yes, TLS is required
 const serverConfig = {
   key: fs.readFileSync('keyVics.pem'),
   cert: fs.readFileSync('certVics.pem'),
 };
 
-app.get('/', (req, res) => {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.end(fs.readFileSync('index.html'));
-});
+// ----------------------------------------------------------------------------------------
 
-// Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
-// [END gae_node_request_example]
+// Create a server for the client html page
 const handleRequest = function(request, response) {
   // Render the single client html file for any request the HTTP server receives
   console.log('request received: ' + request.url);
 
-  };
-  
+  if(request.url === '/') {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(fs.readFileSync('index.html'));
+  } else if(request.url === '/webrtc.js') {
+    response.writeHead(200, {'Content-Type': 'application/javascript'});
+    response.end(fs.readFileSync('client/webrtc.js'));
+  }
+};
+
 const httpsServer = https.createServer(serverConfig, handleRequest);
 httpsServer.listen(HTTPS_PORT, '0.0.0.0');
 
@@ -79,6 +57,3 @@ Some important notes:\n\
   * You\'ll also need to accept the invalid TLS certificate.\n\
   * Some browsers or OSs may not allow the webcam to be used by multiple pages at once. You may need to use two different browsers or machines.\n'
 );
-
-module.exports = app;
-
